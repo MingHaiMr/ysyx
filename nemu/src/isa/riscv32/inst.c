aef37dce5d1510649cde8441d70ee7c5b6303363
MingHaiMr <MingHaiMr@github.com>
2024-07-22 21:42:06 +0800

    modified:   nemu/src/isa/riscv32/inst.c 	modified:   npc/csrc/include/common.h

--- nemu/src/isa/riscv32/inst.c
+++ nemu/src/isa/riscv32/inst.c
@@ -119,7 +119,7 @@ static int decode_exec(Decode *s) {
   INSTPAT("0100000 ????? ????? 101 ????? 01100 11", sra    , R, R(rd) = shamt(src1, BITS(src2, 4, 0)));
 
   
-  INSTPAT("0000000 00001 00000 000 00000 11100` 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
+  INSTPAT("0000000 00001 00000 000 00000 11100 11", ebreak , N, NEMUTRAP(s->pc, R(10))); // R(10) is $a0
   INSTPAT("??????? ????? ????? ??? ????? ????? ??", inv    , N, INV(s->pc));
   INSTPAT_END();
 
